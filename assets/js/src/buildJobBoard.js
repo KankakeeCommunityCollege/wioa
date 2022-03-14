d4988eb6ce94e6eda16f572d9e6c26edbf67e0d4
wdzajicek <tig.welder4@gmail.com>
2022-03-14 16:37:41 -0500

    Add dataTables plugin to job board's table element.

--- assets/js/src/buildJobBoard.js
+++ assets/js/src/buildJobBoard.js
@@ -1,4 +1,9 @@
 const PARENT = document.getElementById('JobBoard');
+const DATA_TABLES_CONFIG = {
+  responsive: true, // Activate responsive powers GO!
+  paging: false, // Don't paginate. Should all be on one page
+  'order': []//, // Initial column ordering
+}
 
 function createRows(data) {
   let html = '';
@@ -29,21 +34,29 @@ function createRows(data) {
       email
     ] = row;
     date = date.replace(/^(\d{4})-(\d{2})-(\d{2}).+$/, `$2/$3/$1`);
-    return html += `<tr><td>${title}</td><td>${coName}</td><td>${loc}</td><td>${date}</td><td><button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#jobModal${i}">More&nbsp;details...</button></td></tr>`;
+    return html += `<tr>
+  <td>${title}</td>
+  <td>${coName}</td>
+  <td>${loc}</td>
+  <td>${date}</td>
+  <td><button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#jobModal${i}">
+      More&nbsp;details...</button>
+  </td>
+</tr>`;
   });
   return html;
 }
 
 function createTable(data, createJobModals) {
   let html = `<div class="col-xl-10 offset-xl-1 mt-3 mb-4">
-  <table class="table table-sm table-striped table-hover">
+  <table id="JobsTable" class="table table-sm table-striped table-hover">
     <thead>
       <tr>
-        <th>Job Title:</th>
+        <th class="all">Job Title:</th>
         <th>Company:</th>
         <th>Location:</th>
         <th>Date Posted:</th>
-        <th>Details:</th>
+        <th class="all">Details:</th>
       </tr>
     </thead>
     <tbody>
@@ -73,6 +86,12 @@ function buildJobBoard(data) {
     }).then(() => {
       // Async step 3.) create modals with the full job details.
       return import('./createJobModals').then(({default: createJobModals}) => createJobModals(data))
+    }).then(() => {
+      // Async step 4.) Do not attempt to access `table#JobsTable`, prior to step 1.)
+      // Initiate DataTables
+      const TABLE = document.getElementById('JobsTable');
+
+      return $(TABLE).DataTable(DATA_TABLES_CONFIG);
     })
 }
 
