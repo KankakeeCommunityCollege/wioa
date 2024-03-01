15e77267f37e27d36fe0581e35b748e18bbf66dd
wdzajicek <tig.welder4@gmail.com>
2024-03-01 09:21:31 -0600

    [FIX]: custom JS module `iframeLink.js`...
    * Jotform changed the ID attribute value breaking
      our custom JS.
    * Switch querySelector to something that should be
      more consistent and less subject to change.

--- assets/js/src/iframeLink.js
+++ assets/js/src/iframeLink.js
@@ -2,12 +2,13 @@
  * 
  * 
  *  Custom JS to scroll the JotFrom into view after clicking the "Go to application" button, OR
- *    if the window.location.hash === '#JotFormIFrame-62244641777966' it scrolls the frame into view also.
+ *    if the window.location.hash === '#62244641777966' it scrolls the frame into view also.
  *  Module only loads on the training-grant-application page
  * 
  */
 const BUTTON = document.getElementById('iframeLink');
-
+// iframe ID has changed before so this seems to be a safer way to target the Jotform iframe
+const IFRAME = document.querySelector('iframe[src^="https://form.jotform.com"]');
 /**
  * 
  * @returns {number} the calculated height of the two navigation bars (needed because they are position: fixed;)
@@ -37,17 +38,12 @@ function getScrollOptions(element) {
 function iframeLink() {
   BUTTON.addEventListener('click', e => {
     e.preventDefault();
-    const path = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
-    const target = e.target.href.replace(path, '');
-    const frame = document.querySelector(target);
 
-    window.scrollTo(getScrollOptions(frame));
+    window.scrollTo(getScrollOptions(IFRAME));
   });
 
-  if (window.location.hash === '#JotFormIFrame-62244641777966') {
-    const frame = document.querySelector(window.location.hash);
-
-    window.scrollTo(getScrollOptions(frame));
+  if (window.location.hash === '#62244641777966') {
+    window.scrollTo(getScrollOptions(IFRAME));
   }
 }
 
